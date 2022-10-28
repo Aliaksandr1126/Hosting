dc2d833eb5a2a7b3d3f8574a2e8a23973e53f9dc
Aliaksandr1126 <95302699+Aliaksandr1126@users.noreply.github.com>
2022-10-28 19:37:39 +0300

    Hosting 3.0

---
+++ assets/js/app.js
@@ -0,0 +1,83 @@
+$(function() {
+    let intro = $("#intro");
+    let header = $("#header");
+    let introH = intro.innerHeight();
+    let headerH = header.innerHeight();
+    let scrollTop = $(window).scrollTop();
+
+    // Header scroll
+
+$(window).on("scroll resize", function() {
+
+    let scrollTop = $(this).scrollTop();
+    if( scrollTop >= (introH - headerH) ) {
+        header.addClass("header__one");
+    } else {
+        header.removeClass("header__one");
+    }
+
+    });
+
+    // Smooth scrolling sections
+    $("[data-scroll]").on("click", function(event) {
+        event.preventDefault();
+
+        let scrollEl = $(this).data("scroll");
+        let scrollElPos = $(scrollEl).offset().top;
+
+        $("html, body").animate({
+            scrollTop: scrollElPos - headerH
+        }, 1000)
+
+        console.log(scrollElPos);
+    });
+
+
+    // Scroll Spy
+    let windowH = $(window).height();
+
+    scrollSpy(scrollTop);
+
+    $(window).on("scroll", function() {
+        scrollTop = $(this).scrollTop();
+
+        scrollSpy(scrollTop);
+    });
+    
+    function scrollSpy(scrollTop) {
+        $("[data-scrollspy]").each(function() {
+
+           let $this = $(this);
+           let sectionId = $(this).data('scrollspy');
+           let sectionOffset = $this.offset().top;
+           sectionOffset = sectionOffset - (windowH * 0.3)
+    
+           if(scrollTop >= sectionOffset) {
+            $('#nav [data-scroll').removeClass('active');
+    
+            $('#nav [data-scroll="' + sectionId + '"]').addClass('active');
+            }
+    
+            if(scrollTop == 0) {
+                $('#nav [data-scroll').removeClass('active');
+                }
+            });        
+        }
+
+
+        // Slick slider https://kenwheeler.github.io/slick/
+        
+        $("#introSlider").slick({
+            infinite: true,
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            arrows: false,
+            fade: true,
+            autoplay: true,
+            autoplaySpeed: 5000
+          });
+
+});
+
+
+
